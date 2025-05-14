aada24dacdb8c9ea96e051d2d3e60ccebc70c223
Soumy Gaur <155235016+soumyagaur3@users.noreply.github.com>
2025-05-14 09:43:10 +0530

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,29 @@
+#Data Preparation
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import datetime
+
+df = pd.read_csv('delhiaqi.csv', parse_dates=['date'])
+
+print(df.info())
+print(df.describe())
+
+print(df.isnull().sum())
+
+#Time Series Analysis
+df.set_index('date', inplace=True)
+
+plt.figure(figsize=(15, 20))
+for i, col in enumerate(df.columns, 1):
+    plt.subplot(4, 2, i)
+    plt.plot(df[col])
+    plt.title(col)
+    plt.tight_layout()
+plt.show()
+
+# Pollutant Correlation 
+plt.figure(figsize=(10, 8))
+sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
+plt.title('Pollutant Correlation Matrix')
+plt.show()
